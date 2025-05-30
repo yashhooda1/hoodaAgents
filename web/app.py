6572d734dc3052946f93e9731eb7c77b1a39d854
Yash Hooda <67335307+yashhooda1@users.noreply.github.com>
2025-05-30 07:12:34 -0400

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from dotenv import load_dotenv
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from langchain_community.chat_models import ChatOllama
